89f9c33386462c71685565a86500b15432f927ba
Dishant Kachhadiya <dishant_kachhadiya@LAPTOP-DCOP102U.localdomain>
2026-06-28 17:07:23 +0000

    Node-Health file Uploaded

---
+++ Node-Health.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Author : Dishant Kachhadiya 
+#Used : Used for checking the health and status of node 
+#Version : V1 
+
+#Seperate the below commands
+
+set -x 
+
+
+df -h 
+
+free -g 
+
+nproc  
